76c6058d614103289483d00620798420e73358c1
kevinthomas <kevvin.thomas@gmail.com>
2024-07-22 13:19:56 +0530

    fixed mobile navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -4,6 +4,34 @@ import logo from '@/images/svb-logo.png'
 import Image from 'next/image'
 import { MenuIcon, XIcon } from '@heroicons/react/outline'
 
+const buttons = [
+  {
+    title : "Channels",
+    link : "/channels",
+    primary : true
+  },
+  {
+    title : "Studios",
+    link : "/studios",
+    primary : true
+  },
+  {
+    title : "Advertise",
+    link : "/advertise",
+    primary : true
+  },
+  {
+    title : "Careers",
+    link : "/careers",
+    primary : true
+  },
+  {
+    title : "Contact Us",
+    link : "/contact",
+    primary : false
+  },
+]
+
 const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false)
 
@@ -13,7 +41,7 @@ const NavBar = () => {
 
   return (
     <div className=''>
-      <div className={`flex flex-col md:flex-row lg:flex-row justify-between items-center py-5 px-6 transition-all ease-in-out duration-500 ${isOpen && 'justify-between'}`}>
+      <div className={`flex flex-col md:flex-row lg:flex-row justify-between items-center py-5 px-3 md:px-6 transition-all ease-in-out duration-500 ${isOpen && 'justify-between'}`}>
         <div className='flex justify-between items-center w-full md:w-auto'>
           <a href='/'>
             <Image src={logo} className='w-44 object-contain' />
@@ -25,31 +53,26 @@ const NavBar = () => {
           </div>
         </div>
         <div className={`${ isOpen ? 'block' : 'hidden' } md:flex flex-col md:flex-row items-center gap-5 text-sm mt-5 md:mt-0 w-full md:w-auto`}>
-          <a href="/channels">
-            <button className='bg-[#d9d9d9] bg-opacity-30 hover:bg-opacity-50 transition-all py-1 px-3 text-white rounded-lg font-regular'>
-              CHANNELS
-            </button>
-          </a>
-          <a href="/studios">
-            <button className='bg-[#d9d9d9] bg-opacity-30 hover:bg-opacity-50 transition-all py-1 px-3 text-white rounded-lg font-regular'>
-              STUDIOS
-            </button>
-          </a>
-          <a href="/advertisements">
-            <button className='bg-[#d9d9d9] bg-opacity-30 hover:bg-opacity-50 transition-all py-1 px-3 text-white rounded-lg font-regular'>
-              ADVERTISE
-            </button>
-          </a>
-          <a href="/careers">
-            <button className='bg-[#d9d9d9] bg-opacity-30 hover:bg-opacity-50 transition-all py-1 px-3 text-white rounded-lg font-regular'>
-              CAREERS
-            </button>
-          </a>
-          <a href="/contact">
-            <button className='bg-white py-1 px-3 text-black rounded-lg font-regular'>
-              CONTACT US
-            </button>
-          </a>
+
+          {buttons.map((button) => {
+            if(button.primary){
+            return(
+              <a href={button.link}>
+                <button className='bg-[#d9d9d9] uppercase bg-opacity-60 backdrop-blur font-medium w-1/4 text-xs md:text-md md:w-auto md:bg-opacity-30 hover:bg-opacity-50 m-1 md:m-0 transition-all py-1 md:px-3 text-white rounded-lg font-regular'>
+                  {button.title}
+                </button>
+              </a>
+            )
+          }else{
+            return(
+              <a href={button.link}>
+                <button className='bg-white text-xs md:text-md py-1 w-auto px-3 font-medium uppercase m-2 md:m-0 text-black rounded-lg'>
+                  {button.title}
+                </button>
+              </a>
+            )
+          }
+          })}
         </div>
       </div>
     </div>
