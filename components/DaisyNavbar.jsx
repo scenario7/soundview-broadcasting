722d1394f92bfe1f3062b7225bcecaf644164a58
kevinthomas <kevvin.thomas@gmail.com>
2024-08-05 09:16:56 +0530

    navbar theme issue

--- components/DaisyNavbar.jsx
+++ components/DaisyNavbar.jsx
@@ -6,7 +6,7 @@ import { channels } from '@/app/ChannelData'
 
 const DaisyNavbar = () => {
   return (
-    <div className="navbar bg-base-100 md:px-10 md:py-5">
+    <div className="text-white navbar bg-base-100 md:px-10 md:py-5">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
